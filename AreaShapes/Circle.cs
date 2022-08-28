b524b5b8f41c162e110880c33baf521ee126620c
Dmitriy Kryhtin <dmitriy@Dmitriys-MacBook-Pro.local>
2022-08-28 13:13:59 +0600

    Add class Circle

---
+++ AreaShapes/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaShapes
+{
+    public class Circle : Shape
+    {
+        readonly ulong radius;
+
+        public Circle(ulong radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double Square
+        {
+            get
+            {
+                return Math.PI * Math.Pow(radius, 2);
+            }
+        }
+    }
+}
