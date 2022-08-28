7c72ef8b50254043db9261ca882b4d7a31456ec5
Dmitriy Kryhtin <dmitriy@Dmitriys-MacBook-Pro.local>
2022-08-28 13:13:37 +0600

    Add class Triangle

--- AreaShapes/Triangle.cs
+++ AreaShapes/Triangle.cs
@@ -1,6 +1,38 @@
-﻿namespace AreaShapes;
-public class Class1
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace AreaShapes
 {
+    public class Triangle : Shape, ITriangle
+    {
+        public ulong A;
+        public ulong B;
+        public ulong C;
 
+        public Triangle(ulong a, ulong b, ulong c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public override double Square
+        {
+            get
+            {
+                var semiPerimeter = (A + B + C) / 2;
+                return Math.Sqrt(semiPerimeter
+                    * ((semiPerimeter - A)
+                    * (semiPerimeter - B)
+                    * (semiPerimeter - C)));
+            }
+        }
+
+        public bool IsRight()
+        {
+            return ((A * A == B * B + C * C)
+                || (B * B == A * A + C * C)
+                || (C * C == A * A + B * B));
+        }
+    }
 }
-
